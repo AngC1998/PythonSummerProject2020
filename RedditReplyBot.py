cecb380927acd182011ef2af42b326115512f439
Angela Cao <runningnerdtroll@gmail.com>
2020-08-19 10:45:38 -0500

    added flexibility to reply bot

--- RedditReplyBot.py
+++ RedditReplyBot.py
@@ -22,10 +22,22 @@ else:
 #Have the bot say different things
 #Add flexibility for subreddits
 subreddit = reddit.subreddit('UTAustin')
-for submission in subreddit.hot(limit=5):
+for submission in subreddit.hot():
     if submission.id not in posts_replied_to:
         if "CS" in submission.title:
             submission.reply("I am a bot, and I love UTCS. Hook em Horns")
+            print('Bot replying to: ', submission.title)
+            posts_replied_to.append(submission.id)
+        elif "UT" in submission.title:
+            submission.reply("Hook 'em horns!")
+            print('Bot replying to: ', submission.title)
+            posts_replied_to.append(submission.id)
+
+subreddit_two = reddit.subreddit('kpop')
+for submission in subreddit_two.hot():
+    if submission.id not in posts_replied_to:
+        if "ONEUS" in submission.title:
+            submission.reply("The recent ONEUS comeback was great. Their choreography was awesome and their voices sound nice.")
             print('Bot replying to: ', submission.title)
             posts_replied_to.append(submission.id)
 
