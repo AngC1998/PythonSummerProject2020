44adea554010909844d4d4d7fcbf23a49a2fb219
Angela Cao <runningnerdtroll@gmail.com>
2020-08-20 15:34:34 -0500

    added flexibility to the reply bot overall through function close #21

--- RedditReplyBot.py
+++ RedditReplyBot.py
@@ -3,6 +3,7 @@ import pdb
 import re
 import os
 from botKeys import *
+import random
 
 #Create the Reddit instance
 reddit = praw.Reddit(client_id=client_id,
@@ -21,6 +22,7 @@ else:
 
 #Have the bot say different things
 #Add flexibility for subreddits
+'''
 subreddit = reddit.subreddit('UTAustin')
 for submission in subreddit.hot():
     if submission.id not in posts_replied_to:
@@ -40,21 +42,41 @@ for submission in subreddit_two.hot():
             submission.reply("The recent ONEUS comeback was great. Their choreography was awesome and their voices sound nice.")
             print('Bot replying to: ', submission.title)
             posts_replied_to.append(submission.id)
+'''
 
-page = 'hello'
-
-while page != '-1':
-    page = input('Enter in subreddit: ')
-    keyword = input('Enter in keyword in title you want to find: ')
-    reply = input('Enter what response you want to generate: ')
-
+def reply_bot(page, keywords, responses):
     subreddit_page = reddit.subreddit(page)
     for submission in subreddit_page.hot():
         if submission.id not in posts_replied_to:
-            if keyword in submission.title:
-                submission.reply(reply)
-                print('Bot replying to: ', submission.title)
-                posts_replied_to.append(submission.id)
+            for keyword in keywords:
+                if keyword in submission.title:
+                    index = random.randint(0, len(responses)-1)
+                    reply = responses[index]
+                    submission.reply(reply)
+                    print('Bot replying to: ', submission.title)
+                    posts_replied_to.append(submission.id)
+
+page = 'hello'
+keywords = []
+responses = []
+
+while page != '-1':
+    page = input('Enter in subreddit: ')
+    keyword = 'hello'
+    reply = 'hello'
+    while keyword != '-1':
+        keyword = input('Enter in keyword in title you want to find: ')
+        if keyword != '-1':
+            keywords.append(keyword)
+    while reply != '-1':
+        reply = input('Enter what response you want to generate:')
+        if reply != '-1':
+            responses.append(reply)
+    if page == '-1':
+        print('Reply bot will terminate.')
+    else:
+        reply_bot(page, keywords, responses)
+
 
 with open('posts_replied_to.txt', 'w') as f:
     for post_id in posts_replied_to:
